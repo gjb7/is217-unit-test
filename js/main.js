171434f859e5e0feff241f8a2b24e5b01506356a
Grant J. Butler <butler.grant.j@gmail.com>
2014-02-11 23:38:14 -0500

    Updated tests to work with DOM.

--- js/main.js
+++ js/main.js
@@ -14,5 +14,25 @@
 		date("2008/01/26 22:23:30", "2 days ago");
 		date("2007/01/26 22:23:30", null);
 	});
+	
+	QUnit.test("prettyDate.update", function() {
+		var links = $('#qunit-fixture .time a')
+		
+		QUnit.equal(links[0].innerHTML, "January 28th, 2008");
+		QUnit.equal(links[1].innerHTML, "January 27th, 2008");
+		prettyDate.update("2008-01-28T22:25:00Z");
+		QUnit.equal(links[0].innerHTML, "2 hrs ago");
+		QUnit.equal(links[1].innerHTML, "yesterday");
+	});
+	
+	QUnit.test("prettyDate.update, one day later", function() {
+		var links = $('#qunit-fixture .time a')
+		
+		QUnit.equal(links[0].innerHTML, "January 28th, 2008");
+		QUnit.equal(links[1].innerHTML, "January 27th, 2008");
+		prettyDate.update("2008/01/29 22:25:00");
+		QUnit.equal(links[0].innerHTML, "yesterday");
+		QUnit.equal(links[1].innerHTML, "2 days ago");
+	});
 })(window);
 
