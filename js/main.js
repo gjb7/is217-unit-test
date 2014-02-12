c715b6007ec221ce5cf00ac648ae683b629440b4
Grant J. Butler <butler.grant.j@gmail.com>
2014-02-11 23:20:21 -0500

    Reworked to do testing via console.

--- js/main.js
+++ js/main.js
@@ -41,16 +41,26 @@
 		return null;
 	}
 	
-	$(function() {
-		$('a').each(function() {
-			var link = $(this);
-			if (link.attr('title')) {
-				var date = prettyDate('2008-01-28T22:25:00Z', link.attr('title'));
-				if (date) {
-					link.html(date);
-				}
-			}
-		});
-	});
+	function test(then, expected) {
+		results.total++;
+		var result = prettyDate("2008/01/28 22:25:00", then);
+		if (result !== expected) {
+			results.bad++;
+			console.log("Expected " + expected + ", but was " + result);
+		}
+	}
+	
+	var results = {
+		total: 0,
+		bad: 0
+	};
+	
+	test("2008/01/28 22:24:30", "just now");
+	test("2008/01/28 22:23:30", "1 min ago");
+	test("2008/01/28 21:23:30", "1 hr ago");
+	test("2008/01/27 22:23:30", "yesterday");
+	test("2008/01/26 22:23:30", "2 days ago");
+	test("2007/01/26 22:23:30", null);
+	console.log("Of " + results.total + " tests, " + results.bad + " failed, " + (results.total - results.bad) + " passed.");
 })(window);
 
